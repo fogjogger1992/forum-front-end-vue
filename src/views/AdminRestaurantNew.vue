9aba0f9e25811be76779da2a9dbc3f17d907e2f0
EJ Tseng <fogjogger1992@gmail.com>
2021-06-28 11:29:51 +0800

    feat: Add AdminRestaurantForm

---
+++ src/views/AdminRestaurantNew.vue
@@ -0,0 +1,26 @@
+// ./src/views/AdminRestaurantNew.vue
+<template>
+  <div class="container py-5">
+    <!-- 餐廳表單 AdminRestaurantForm -->
+    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from "./../components/AdminRestaurantForm.vue";
+
+export default {
+  name: "AdminRestaurantNew",
+  components: {
+    AdminRestaurantForm,
+  },
+  methods: {
+    handleAfterSubmit(formData) {
+      // TODO: 透過API把formData資料往後端伺服器送
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ": " + value);
+      }
+    },
+  },
+};
+</script>
