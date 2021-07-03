afd3d3cdec6ba836a8f79e1eca7fc78f9e52d948
EJ Tseng <fogjogger1992@gmail.com>
2021-07-03 22:10:13 +0800

    refactor: Replace getToken in APIs

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,13 +2,26 @@ import axios from 'axios'
 import Swal from 'sweetalert2'
 
 const baseURL = 'http://localhost:3000/api'
-export const apiHelper = axios.create({
-  baseURL
+const axiosInstance = axios.create({
+  baseURL,
 })
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+  },
+  (err) => Promise.reject(err)
+)
+
+export const apiHelper = axiosInstance
 
 export const Toast = Swal.mixin({
   toast: true,
   position: 'top-end',
   showConfirmButton: false,
-  timer: 3000
+  timer: 3000,
 })
